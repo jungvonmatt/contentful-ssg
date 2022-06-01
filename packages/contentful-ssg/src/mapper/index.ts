4698aca7b3579c310a93c8e1e99d9c9923393e3e
Ben Zörb <benjamin.zoerb@jvm.de>
2022-06-01 12:25:56 +0200

    refactor(cssg-plugin-assets): rewrite in typescript

---
+++ packages/contentful-ssg/src/mapper/index.ts
@@ -0,0 +1,6 @@
+export * from './map-date-field.js';
+export * from './map-entry.js';
+export * from './map-field.js';
+export * from './map-meta-fields.js';
+export * from './map-reference-field.js';
+export * from './map-rich-text-field.js';
---
+++ packages/contentful-ssg/src/tasks/index.ts
@@ -0,0 +1,6 @@
+export * from './fetch.js';
+export * from './localize.js';
+export * from './remove.js';
+export * from './setup.js';
+export * from './transform.js';
+export * from './write.js';
