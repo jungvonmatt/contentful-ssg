dd9ef0e4ac751bac8c0c7423c0459a418eb8280d
Ben Zörb <benjamin.zoerb@jvm.de>
2023-05-13 00:11:21 +0200

    feat(contentful-typings): adapt new contentful skeleton types

--- packages/contentful-typings/src/index.ts
+++ packages/contentful-typings/src/index.ts
@@ -1,12 +1,17 @@
 /* _eslint-disable @typescript-eslint/no-unsafe-call */
+import path from 'node:path';
 import { ContentfulConfig, ContentType } from '@jungvonmatt/contentful-ssg';
 import { getConfig } from '@jungvonmatt/contentful-ssg/lib/config';
 import { getEnvironment, pagedGet } from '@jungvonmatt/contentful-ssg/lib/contentful';
 import { askMissing } from '@jungvonmatt/contentful-ssg/lib/ui';
 import { CFDefinitionsBuilder, ContentTypeRenderer } from 'cf-content-types-generator';
+import { readPackageUp } from 'read-pkg-up';
+import semiver from 'semiver';
 
 import {
   DefaultContentTypeRenderer,
+  V10ContentTypeRenderer,
+  V10TypeGuardRenderer,
   JsDocRenderer,
   LocalizedContentTypeRenderer,
   TypeGuardRenderer,
@@ -16,23 +21,45 @@ type Options = {
   localized?: boolean;
   jsdoc?: boolean;
   typeguard?: boolean;
+  legacy?: boolean;
+};
+
+const isLegacyVersion = async (dir?: string) => {
+  try {
+    const cwd = path.join(dir || process.cwd(), 'node_modules', 'contentful');
+    const { packageJson } = await readPackageUp({ cwd });
+    // New skeleton types were released in contentful v10.0.0-beta-v10.33
+    if (
+      packageJson.name === 'contentful' &&
+      semiver(packageJson.version, '10.0.0-beta-v10.33') < 0
+    ) {
+      return true;
+    }
+  } catch {}
+
+  return false;
 };
 
 export const generateTypings = async (options: Options = {}) => {
-  const contentfulConfig: ContentfulConfig = (await askMissing(
+  const contentfulConfig = await askMissing(
     await getConfig({
       previewAccessToken: '-',
       accessToken: '-',
     })
-  )) as ContentfulConfig;
+  );
 
-  const client = await getEnvironment(contentfulConfig);
+  const client = await getEnvironment(contentfulConfig as ContentfulConfig);
 
   const { items: contentTypes } = await pagedGet<ContentType>(client, {
     method: 'getContentTypes',
   });
 
-  const renderers: ContentTypeRenderer[] = [new DefaultContentTypeRenderer()];
+  const legacyVersion =
+    typeof options.legacy === 'undefined' ? await isLegacyVersion() : options.legacy;
+
+  const renderers: ContentTypeRenderer[] = [
+    legacyVersion ? new DefaultContentTypeRenderer() : new V10ContentTypeRenderer(),
+  ];
   if (options.localized) {
     renderers.push(new LocalizedContentTypeRenderer());
   }
@@ -42,7 +69,7 @@ export const generateTypings = async (options: Options = {}) => {
   }
 
   if (options.typeguard) {
-    renderers.push(new TypeGuardRenderer());
+    renderers.push(legacyVersion ? new TypeGuardRenderer() : new V10TypeGuardRenderer());
   }
 
   const builder = new CFDefinitionsBuilder(renderers);
--- packages/contentful-typings/src/renderer/contentTypeRenderer.ts
+++ packages/contentful-typings/src/renderer/contentTypeRenderer.ts
@@ -1,11 +1,18 @@
 import {
   DefaultContentTypeRenderer as DefaultContentTypeRendererOriginal,
+  V10ContentTypeRenderer as V10ContentTypeRendererOriginal,
   RenderContext,
 } from 'cf-content-types-generator';
-import { context } from './context.js';
+import { context, v10context } from './context.js';
 
 export class DefaultContentTypeRenderer extends DefaultContentTypeRendererOriginal {
   public createContext(): RenderContext {
     return context;
+  }
+}
+
+export class V10ContentTypeRenderer extends V10ContentTypeRendererOriginal {
+  public createContext(): RenderContext {
+    return v10context;
   }
 }
