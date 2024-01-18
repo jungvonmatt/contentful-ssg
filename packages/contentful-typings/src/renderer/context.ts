b1f0ead26c4db0c05e0c4394f2622e6143c2850b
Ben Zörb <ben@sommerlaune.com>
2024-01-18 01:10:59 +0100

    fix(deps): bump dependencies

--- packages/contentful-typings/src/renderer/context.ts
+++ packages/contentful-typings/src/renderer/context.ts
@@ -1,7 +1,7 @@
-import camelcase from 'camelcase';
+import { pascalCase } from 'change-case';
 import { createDefaultContext, createV10Context, RenderContext } from 'cf-content-types-generator';
 
-export const moduleName = (name: string) => `${camelcase(name, { pascalCase: true })}`;
+export const moduleName = (name: string) => `${pascalCase(name)}`;
 export const moduleFieldsName = (name: string) => `${moduleName(name)}Fields`;
 export const moduleSkeletonName = (name: string): string => `${moduleName(name)}Skeleton`;
 export const context: RenderContext = {
